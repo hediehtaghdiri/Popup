f996546de943d28c1646097f6da5bcebacf8e29b
hedieh <141828190+hediehtaghdiri@users.noreply.github.com>
2024-02-01 17:36:43 +0330

    Add files via upload

---
+++ pupop/script.js
@@ -0,0 +1,12 @@
+let Popup=document.getElementById("dv2")
+
+function Openpopup(){
+
+Popup.classList.add('open-popup');
+
+}
+function Closepopup(){
+
+    Popup.classList.remove('open-popup');
+    
+    }
